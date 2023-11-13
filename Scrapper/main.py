612ac3c8305d60fc1e03ffbd8945e193bb3e56e0
Tarixxiv <r.litwin@interia.pl>
2023-11-13 22:12:48 +0100

    refactor: moved file management into class

--- Scrapper/main.py
+++ Scrapper/main.py
@@ -7,9 +7,16 @@ import requests
 class Scrapper:
     def __init__(self):
         self.soup = BeautifulSoup()
+        self.categoriesAndSubcategoriesFile = 'categoriesAndSubcategories'
+        self.productsFile = 'products'
+        self.url = ''
 
     def set_soup_from_page(self, url):
+        self.url = url
         self.soup = BeautifulSoup(self.get_page_text(url), 'lxml')
+
+    def get_url_without_php(self):
+        return self.url.split('?', 2)[0]
 
     @staticmethod
     def get_page_text(url):
@@ -45,39 +52,44 @@ class Scrapper:
         text = self.get_one_content('span', 'sc-11oikyw-2 fUaLpF', 'page count').text
         return int(text.split(' ')[1])
 
-
-if __name__ == "__main__":
-    scraper = Scrapper()
-    host = 'https://www.x-kom.pl/'
-    path = 'g-5/c/345-karty-graficzne.html'
-    categoriesAndSubcategoriesFile = 'categoriesAndSubcategories'
-    productsFile = 'products'
-
-    with open(categoriesAndSubcategoriesFile, 'w'):
-        pass
-    with open(productsFile, 'w'):
-        pass
-    scraper.set_soup_from_page(host + path)
-    page_count = scraper.get_page_count()
-
-    for page_number in range(page_count):
-        page_php = "?page=" + str(page_number + 1)
-        scraper.set_soup_from_page(host + path + page_php)
-        categories = scraper.get_all_content('span', 'sc-1fme39r-4 hkrryw', 'categories')
-        with open(categoriesAndSubcategoriesFile, 'a') as file:
+    def append_categories_to_file(self):
+        categories = self.get_all_content('span', 'sc-1fme39r-4 hkrryw', 'categories')
+        with open(self.categoriesAndSubcategoriesFile, 'a') as file:
             for cat in categories:
                 file.write(cat.text + ",")
             file.write("\n")
 
-        products = scraper.get_all_content('div', 'sc-1s1zksu-0 dzLiED sc-162ysh3-1 irFnoT', 'products')
-        with open(productsFile, 'a') as file:
-            for product in products:
-                name = product.find('h3', class_='sc-16zrtke-0 kGLNun sc-1yu46qn-9 feSnpB')
-                file.write(name.text + ",")
-                attributes = product.find_all('li', 'sc-vb9gxz-2 ZaTQK')
-                for att in attributes:
-                    file.write(att.text + ",")
-                image = product.find('img')
-                print(image['src'])
-                file.write(image['src'])
-                file.write("\n")
+    def append_products_to_file(self):
+        page_count = scrapper.get_page_count()
+        for page_number in range(page_count):
+            page_php = "?page=" + str(page_number + 1)
+            self.set_soup_from_page(self.get_url_without_php() + page_php)
+            products = scrapper.get_all_content('div', 'sc-1s1zksu-0 dzLiED sc-162ysh3-1 irFnoT', 'products')
+            with open(self.productsFile, 'a') as file:
+                for product in products:
+                    name = product.find('h3', class_='sc-16zrtke-0 kGLNun sc-1yu46qn-9 feSnpB')
+                    file.write(name.text + ",")
+                    attributes = product.find_all('li', 'sc-vb9gxz-2 ZaTQK')
+                    for att in attributes:
+                        file.write(att.text + ",")
+                    image = product.find('img')
+                    print(image['src'])
+                    file.write(image['src'])
+                    file.write("\n")
+
+    def clear_files(self):
+        with open(self.categoriesAndSubcategoriesFile, 'w'):
+            pass
+        with open(self.productsFile, 'w'):
+            pass
+
+
+if __name__ == "__main__":
+    scrapper = Scrapper()
+    host = 'https://www.x-kom.pl/'
+    path = 'g-5/c/345-karty-graficzne.html'
+    scrapper.clear_files()
+
+    scrapper.set_soup_from_page(host + path)
+    scrapper.append_categories_to_file()
+    scrapper.append_products_to_file()
