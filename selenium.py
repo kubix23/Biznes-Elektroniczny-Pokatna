704a3cf36b4af41054aff6d93620c31c9fe519e4
Khasar0451 <lucker10@interia.pl>
2023-12-05 16:17:27 +0100

    Update selenium.py
    Cart's tests sucessful

--- selenium.py
+++ selenium.py
@@ -1,3 +1,5 @@
+import random
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -10,28 +12,26 @@ class Tester:
         self.url = url
         self.driver = webdriver.Chrome()
         self.driver.get(self.url)
-
-    def add_to_cart_test(self):
+    def add_to_cart_test(self, cat):
         collapse_menu = self.driver.find_element(by=By.CLASS_NAME, value="top-menu")
         action = ActionChains(driver=self.driver)
         action.move_to_element(collapse_menu).perform()
-        cat1 = self.driver.find_element(by=By.LINK_TEXT, value="Karty graficzne AMD")
+        cat1 = self.driver.find_element(by=By.LINK_TEXT, value=cat)
         action.move_to_element(cat1).click().perform()
 
         for i in range(5):
-            products = self.driver.find_elements(by=By.CLASS_NAME, value="product-description")
-            print(products[i].text)
+            products = self.driver.find_elements(by=By.CLASS_NAME, value="product-title")
             products[i].click()
-            # WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CLASS_NAME,"btn btn-primary add-to-cart"))).click()
-            # self.driver.implicitly_wait(3)
-
-            action.move_to_element(self.driver.find_element(by=By.XPATH, value="//*[@id=\"add-to-cart-or-refresh\"]/div[2]/div/div[2]/button")).click().perform()
-
-            # action.click().perform()
+            additional_product_amount = random.randint(0,4)
+            for j in range(additional_product_amount):
+                self.driver.find_element(by=By.CLASS_NAME, value="touchspin-up").click()
+            self.driver.find_element(by=By.CLASS_NAME, value="add-to-cart").click()
             self.driver.back()
+
+
 if __name__ == "__main__":
-    #host = 'http://localhost:8080/index.php'
-    host = 'file:///home/khasar/Desktop/Karty%20graficzne%20NVIDIA.html'
+    host = 'http://localhost:8080/index.php'
     tester = Tester(host)
-    tester.add_to_cart_test()
+    tester.add_to_cart_test("Karty graficzne AMD")
+    tester.add_to_cart_test("Płyty główne Socket AM4")
     tester.driver.quit()
