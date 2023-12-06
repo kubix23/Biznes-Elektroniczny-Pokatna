d9f3da65a2498e33194792644bef40061ce73ea4
Khasar0451 <lucker10@interia.pl>
2023-12-06 12:48:58 +0100

    test b complete

--- selenium.py
+++ selenium.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 
 class Tester:
@@ -12,6 +13,17 @@ class Tester:
         self.url = url
         self.driver = webdriver.Chrome()
         self.driver.get(self.url)
+
+    def search_test(self):
+        search_box = self.driver.find_element(by=By.NAME, value="s")
+        search_box.send_keys("rx 6700")
+        search_box.send_keys(Keys.ENTER)
+
+        products = self.driver.find_elements(by=By.CLASS_NAME, value="product-title")
+        products[random.randint(0, len(products)-1)].click()
+
+        self.driver.find_element(by=By.CLASS_NAME, value="add-to-cart").click()
+
     def add_to_cart_test(self, cat):
         collapse_menu = self.driver.find_element(by=By.CLASS_NAME, value="top-menu")
         action = ActionChains(driver=self.driver)
@@ -22,7 +34,7 @@ class Tester:
         for i in range(5):
             products = self.driver.find_elements(by=By.CLASS_NAME, value="product-title")
             products[i].click()
-            additional_product_amount = random.randint(0,4)
+            additional_product_amount = random.randint(0, 4)
             for j in range(additional_product_amount):
                 self.driver.find_element(by=By.CLASS_NAME, value="touchspin-up").click()
             self.driver.find_element(by=By.CLASS_NAME, value="add-to-cart").click()
@@ -32,6 +44,10 @@ class Tester:
 if __name__ == "__main__":
     host = 'http://localhost:8080/index.php'
     tester = Tester(host)
-    tester.add_to_cart_test("Karty graficzne AMD")
-    tester.add_to_cart_test("Płyty główne Socket AM4")
+    # test a
+    # tester.add_to_cart_test("Karty graficzne AMD")
+    # tester.add_to_cart_test("Płyty główne Socket AM4")
+    
+    # test b
+    # tester.search_test()
     tester.driver.quit()
